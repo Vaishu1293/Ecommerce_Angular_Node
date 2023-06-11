b8e4484cc5e9c3900efc47f86a9b6e4c325adf46
Vaishu1293 <clearexamseasy@gmail.com>
2023-06-11 20:36:59 +0100

    v10

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://food-vending-frontend.web.app/');
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
